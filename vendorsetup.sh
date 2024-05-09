fa8e8e2635a0a1164b4cd3f547c617008eab5444
develux <103586125+develux44@users.noreply.github.com>
2024-05-09 12:01:04 +0500

    Remove thinlto for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,6 @@
 
 base64 -d device/xiaomi/sm6225-common/configs/camera/secret > device/xiaomi/sm6225-common/configs/camera/st_license.lic
 
-echo "Building with ThinLTO."
-export GLOBAL_THINLTO=true
-export USE_THINLTO_CACHE=true
+# echo "Building with ThinLTO."
+# export GLOBAL_THINLTO=true
+# export USE_THINLTO_CACHE=true
