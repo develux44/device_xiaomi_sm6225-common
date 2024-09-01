4b8e891fa2432dbebeed0c8b83650368e66cb0c5
develux <103586125+develux44@users.noreply.github.com>
2024-09-01 17:44:38 +0500

    Update common.mk

--- common.mk
+++ common.mk
@@ -303,7 +303,8 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
 
 # Phantom process monitoring
 PRODUCT_PRODUCT_PROPERTIES += \
